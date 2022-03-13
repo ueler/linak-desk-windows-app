e6e52165efaebd9a79382e941c664fb77f9af2db
Ueli Ebnöther <ueli.ebnoether@ergon.ch>
2022-03-13 10:59:07 +0100

    Add more settings

---
+++ LinakDeskController/LinakDeskControllerSettings.cs
@@ -0,0 +1,50 @@
+﻿using Windows.Storage;
+
+namespace LinakDeskController
+{
+    public class LinakDeskControllerSettings
+    {
+
+        public short standingHeight { get; set; } = 70;
+        public short sittingHeight { get; set; } = 20;
+
+        public short mainIntervalInMinutes { get; set; } = 60;
+
+        public short standingIntervalInMinutes { get; set; } = 15;
+
+
+        public void saveSettings()
+        {
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values["standingHeight"] = this.standingHeight;
+            localSettings.Values["sittingHeight"] = this.sittingHeight;
+            localSettings.Values["mainIntervalInMinutes"] = this.mainIntervalInMinutes;
+            localSettings.Values["standingIntervalInMinutes"] = this.standingIntervalInMinutes;
+        }
+
+        public void loadSettings()
+        {
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            if (localSettings.Values["standingHeight"] != null)
+            {
+                this.standingHeight = (short)localSettings.Values["standingHeight"];
+            }
+
+            if (localSettings.Values["sittingHeight"] != null)
+            {
+                this.sittingHeight = (short)localSettings.Values["sittingHeight"];
+            }
+
+            if (localSettings.Values["mainIntervalInMinutes"] != null)
+            {
+                this.mainIntervalInMinutes = (short)localSettings.Values["mainIntervalInMinutes"];
+            }
+
+            if (localSettings.Values["standingIntervalInMinutes"] != null)
+            {
+                this.standingIntervalInMinutes = (short)localSettings.Values["standingIntervalInMinutes"];
+            }
+        }
+
+    }
+}
--- LinakDeskController/MainWindow.xaml.cs
+++ LinakDeskController/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -27,30 +29,100 @@ namespace LinakDeskController
     {
         private LinakDeskCommandCoordinator linakDeskCommandCoordinator = new LinakDeskCommandCoordinator();
 
+        private LinakDeskControllerSettings settings;
+
+        private bool settingsApplied = false;
+
         Microsoft.UI.Dispatching.DispatcherQueue dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
 
-        public MainWindow()
+        public MainWindow(LinakDeskControllerSettings settings)
         {
+            this.settings = settings;
+
             this.InitializeComponent();
+            this.resizeWindow();
+            this.applySettings();
 
             this.linakDeskCommandCoordinator.getHeightSubject().Subscribe(height =>
             {
                 dispatcherQueue.TryEnqueue(() =>
                 {
-                    info.Text = "Table height: " + height;
+                    currentHeight.Text = "Current table height: " + Math.Round(height / 100.0) + " cm";
                 });
             });
             this.linakDeskCommandCoordinator.run();
         }
 
-        private void moveUp_Click(object sender, RoutedEventArgs e)
+        private void applySettings()
         {
-            this.linakDeskCommandCoordinator.setDeskTargetHeight(6100);
+            this.sittingHeightNumberBox.Value = this.settings.sittingHeight;
+            this.standingHeightNumberBox.Value = this.settings.standingHeight;
+            this.mainIntervalNumberBox.Value = this.settings.mainIntervalInMinutes;
+            this.standingIntervalNumberBox.Value = this.settings.standingIntervalInMinutes;
+            this.settingsApplied = true;
         }
 
-        private void moveDown_Click(object sender, RoutedEventArgs e)
+        private void resizeWindow()
         {
-            this.linakDeskCommandCoordinator.setDeskTargetHeight(1700);
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+            var size = new Windows.Graphics.SizeInt32();
+            size.Width = 600;
+            size.Height = 600;
+
+            appWindow.Resize(size);
+        }
+
+        private void moveToStandingHeight_Click(object sender, RoutedEventArgs e)
+        {
+            this.linakDeskCommandCoordinator.setDeskTargetHeight(this.settings.standingHeight);
+        }
+
+        private void moveToSittingHeight_Click(object sender, RoutedEventArgs e)
+        {
+            this.linakDeskCommandCoordinator.setDeskTargetHeight(this.settings.sittingHeight);
+        }
+
+        private void standingHeightNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            if (!settingsApplied)
+            {
+                return;
+            }
+            this.settings.standingHeight = Convert.ToInt16(args.NewValue);
+            this.settings.saveSettings();
+        }
+
+        private void sittingHeightNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            if (!settingsApplied)
+            {
+                return;
+            }
+            this.settings.sittingHeight = Convert.ToInt16(args.NewValue);
+            this.settings.saveSettings();
+        }
+
+        private void mainIntervalNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            if (!settingsApplied)
+            {
+                return;
+            }
+            this.settings.mainIntervalInMinutes = Convert.ToInt16(args.NewValue);
+            this.settings.saveSettings();
+        }
+
+        private void standingIntervalNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            if (!settingsApplied)
+            {
+                return;
+            }
+            this.settings.standingIntervalInMinutes = Convert.ToInt16(args.NewValue);
+            this.settings.saveSettings();
         }
     }
 }
